4b30f4daef191fb134d5fb951dd4f17963dfaf4a
Isam Ali <isamali.work@gmail.com>
2026-09-10 00:16:17 +0530

    Fix production API URL

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1763945430" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "321152419" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
